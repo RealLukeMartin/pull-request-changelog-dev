baf428534f467fd9702dc3bc33e02aba94ee7be8
RealLukeMartin <lukemartinwebdev@gmail.com>
2022-02-25 14:43:40 -0600

    initial

---
+++ README.md
@@ -0,0 +1,40 @@
+# Github Action PR Changelog Generator
+
+Github action that creates a changelog on opening or updating the PR.
+
+## Dependencies
+
+To use this action, you need to use it together with `action/checkout` from github.
+We need that to access git changes. See usage example under.
+
+## How it works
+
+Whenever you open a PR to `master` branch, action will compare `master` branch with your branch and
+post a comment to PR with all the changes that are going to be merged to `master` branch.
+
+There are a few assumptions that you should be aware, when you're using this action.
+
+## How use
+
+Create a file `.github/workflows/changelog.yml` with:
+
+```yml
+name: Changelog Generator
+on:
+  pull_request:
+    branches:
+      - master
+    types: [opened, reopened, synchronize]
+
+jobs:
+  changelog:
+    name: Chanegelog Generator
+    runs-on: ubuntu-latest
+    steps:
+      - uses: actions/checkout@v2
+      - uses: etcdigital/pull-request-changelog@master
+        with:
+          token: ${{ secrets.GITHUB_TOKEN }}
+```
+
+[See the result...](https://github.com/etcdigital/pull-request-changelog/pull/1#issuecomment-625586295)
---
+++ src/index.ts
@@ -0,0 +1,105 @@
+import fetch from 'node-fetch';
+import { exec } from '@actions/exec';
+import * as github from '@actions/github';
+import * as core from '@actions/core';
+import makeTemplate from './template';
+import { gitNoTag, changeFiles, getCommits, gitPrume } from './commands';
+
+const pull_request = github.context.payload.pull_request;
+const PR_ID = pull_request.number;
+const URL = pull_request.comments_url;
+const GITHUB_TOKEN = core.getInput('token') || process.env.token;
+
+const postToGit = async (url, key, body) => {
+  const rawResponse = await fetch(url, {
+    method: 'POST',
+    headers: {
+      Authorization: `token ${key}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ body }),
+  });
+  if (!rawResponse.ok) {
+    throw new Error(rawResponse.statusText);
+  }
+  const content = await rawResponse.json();
+  return content;
+};
+
+/**
+ * Action core
+ */
+(async () => {
+  try {
+    if (GITHUB_TOKEN === undefined) {
+      throw new Error('Missing auth thoken');
+    }
+    console.log('Generating changelog....');
+
+    await exec(gitPrume);
+    await exec(gitNoTag);
+
+    // then we fetch the diff and grab the output
+    let commits = {};
+    let commitsStr = '';
+    let myError = '';
+
+    // get diff between master and current branch
+    await exec(getCommits(PR_ID), [], {
+      listeners: {
+        stdout: (data) => {
+          const splitted = data.toString().split('\n');
+          splitted.forEach((item) => {
+            if (item === '') {
+              return;
+            }
+            const sha = item.substr(0, 40);
+            if (sha === '') {
+              return;
+            }
+            const message = item.substr(41);
+            commits[sha] = { message };
+          });
+
+          // remove
+          commitsStr = `${commitsStr}${data.toString()}`;
+        },
+        stderr: (data) => {
+          myError = `${myError}${data.toString()}`;
+        },
+      },
+    });
+
+    // If there were errors, we throw it
+    if (myError !== '') {
+      throw new Error(myError);
+    }
+
+    const shaKeys = Object.keys(commits).map(
+      (sha) =>
+        new Promise((resolve, reject) => {
+          exec(changeFiles(sha), [], {
+            listeners: {
+              stdout: (data) => {
+                commits[sha].files = data
+                  .toString()
+                  .split('\n')
+                  .filter((i) => i);
+                resolve();
+              },
+              stderr: (data) => {
+                myError = `${myError}${data.toString()}`;
+              },
+            },
+          });
+        }),
+    );
+
+    await Promise.all(shaKeys);
+
+    await postToGit(URL, GITHUB_TOKEN, makeTemplate(commits));
+  } catch (e) {
+    console.log(e);
+    process.exit(1);
+  }
+})();
